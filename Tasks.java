b2bd450f52a92710d216d142f7024e1f7f238bee
Alex <best1972@list.ru>
2023-04-06 19:50:53 +0300

    made tasks 1

--- Tasks.java
+++ Tasks.java
@@ -1,8 +1,11 @@
 // Задача 1. Пусть дан LinkedList с несколькими элементами. Реализуйте метод (не void), 
-// который вернет “перевернутый” список. 
+// который вернет “перевернутый” список.
+
+// import java.util.Collections;
 // import java.util.LinkedList;
+
 // public class Tasks {
-//     public static void main(String[] args) throws Exception{
+//     public static void main(String[] args) {
 //         LinkedList<String> ll = new LinkedList<>();
 //         ll.add("zxc");
 //         ll.add("7");
@@ -10,15 +13,7 @@
 //         ll.add("6");
 //         ll.add("cvbn");
 //         System.out.println("Исходный список элементов: " + ll);
-//         LinkedList<String> reversedList = reverseLinkedList(ll);
-//         System.out.println("Перевернутый список: " + reversedList);
-//     }
-//     public static LinkedList<String> reverseLinkedList(LinkedList<String> ll) {
-//         LinkedList<String> reversedList = new LinkedList<>();
-//         for (int i = ll.size() - 1; i >= 0; i--) {
-//             String element = ll.get(i);
-//             reversedList.add(element);
-//         }
-//         return reversedList;
+//         Collections.reverse(ll);
+//         System.out.println("Перевернутый список: " + ll);
 //     }
 // }
