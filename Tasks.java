e22118053c520f18b8f6b27c663e526961334f08
Alex <best1972@list.ru>
2023-04-09 11:12:54 +0300

    made tasks 2

--- Tasks.java
+++ Tasks.java
@@ -2,7 +2,7 @@
 // который вернет “перевернутый” список.
 
 // import java.util.Collections;
-// import java.util.LinkedList;
+import java.util.LinkedList;
 
 // public class Tasks {
 //     public static void main(String[] args) {
@@ -15,5 +15,38 @@
 //         System.out.println("Исходный список элементов: " + ll);
 //         Collections.reverse(ll);
 //         System.out.println("Перевернутый список: " + ll);
+//     }
+// }
+
+// Задача 2. Реализуйте очередь с помощью LinkedList со следующими методами: 
+// enqueue() - помещает элемент в конец очереди, 
+// LinkedListue() - возвращает первый элемент из очереди и удаляет его, 
+// first() - возвращает первый элемент из очереди, не удаляя.  
+
+// import java.util.Scanner;
+// import java.util.Arrays;
+// public class Tasks {
+//     public static void main(String[] args) {
+//         LinkedList<Object> list = new LinkedList<>(Arrays.asList(1,2,3,4,5));
+//         System.out.printf("Cписок элементов: %s \n", list);
+//         Scanner sc = new Scanner(System.in);
+//         System.out.printf("Ввод элемента: ");
+//         enqueue(list, sc.nextInt());
+//         System.out.println("enqueue() - помещает элемент в конец очереди " + list);
+//         System.out.printf("LinkedListue() - возвращает первый элемент - %s из очереди и удаляет его \n", LinkedListue(list));
+//         System.out.printf("Cписок элементов: %s \n", list);
+//         System.out.printf("first() - возвращает первый элемент - %s из очереди, не удаляя его \n", first(list));
+//         System.out.printf("Cписок элементов: %s \n", list);
+//         sc.close();
+//     }
+//     public static void enqueue(LinkedList<Object> list, int element){
+//         list.add(element);
+//     }
+//     public static Object LinkedListue(LinkedList<Object>list){
+//         Object a = list.pollFirst();
+//         return a;
+//     }
+//     public static Object first(LinkedList<Object> list){
+//         return list.getFirst();
 //     }
 // }
